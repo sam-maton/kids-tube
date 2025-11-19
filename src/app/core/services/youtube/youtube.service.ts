3846049a3c291f86ce44a9fcd8620a86f35e9c23
Sam Maton <sam.maton91@gmail.com>
2025-11-19 16:15:31 +0000

    started work on feed page

--- src/app/core/services/youtube/youtube.service.ts
+++ src/app/core/services/youtube/youtube.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, forkJoin, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class YoutubeService {
-  
+
+  //API String for channel searching
+  // https://www.googleapis.com/youtube/v3/search?key=${this.API_KEY}&q=dog%20training&type=channel&part=snippet,id&maxResults=25
+
+  private API_KEY = 'fail'
+
+  constructor(private http: HttpClient) {}
+
+  getChannelVideos(channelId: string): Observable<any> {
+    const apiString = `https://www.googleapis.com/youtube/v3/search?key=${this.API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=10`
+
+    const results = this.http.get<any>(apiString)
+
+    return results
+  }
 }
--- src/app/pages/child/feed/feed.page.ts
+++ src/app/pages/child/feed/feed.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonItem, IonList, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.page.html',
   styleUrls: ['./feed.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonList, CommonModule, FormsModule]
 })
 export class FeedPage implements OnInit {
 
